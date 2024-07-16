66d89d3f54f1b7c9d457760b6993cd86e2a60ab2
PujaPKS <pkpuja2404@gmail.com>
2024-07-17 01:42:16 +0530

    Rendering Expenses so that the entered values in forms gets reflected on the page.

---
+++ 08part/README.md
@@ -0,0 +1,3 @@
+For rendering data we wil use "useState" hook.
+
+"useState" hook returns a array of 2 elements. First is the "latest version of the state" and 2nd is the basically the "hook function which is used to update the state". 
---
+++ 08part/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Expenses from "./components/Expense/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+function App() {
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
+    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
+    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
+    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
+  ]);
+
+  const addExpenseHandler = (expense) => {
+    // console.log(expense);
+    setExpenses((prevExpense) =>{
+      return [expense, ...prevExpense];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
