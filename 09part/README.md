f50cf2d265f4ca8aca3bcb372975533981d6e6dd
PujaPKS <pkpuja2404@gmail.com>
2024-07-17 13:06:33 +0530

    Controlled Components - Expenses Filter applied on the list.

---
+++ 09part/README.md
@@ -0,0 +1,10 @@
+Controlled components are controlled by their parents basically. So child component is Controlled Components.
+
+"Contolled Components" are used basically for UI. Basically some kind of user experiense we will see.
+The logic will be in "Parent Component". And the child component will be controlled by parent component.
+
+For seeing the logic of "Controlled Components" we are adding filtering to the list in our code.
+
+"ExpensesFilter.js" files logic are written in the parent file which is "Expenses.js" file.
+
+"filter" keyword used for filtering year as given by user.
---
+++ 09part/src/components/Expense/Expenses.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
+const Expenses = (props) => {
+  
+  const [filteredYear, setFilteredYear] = useState('2023');
+  
+  const changeFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  //function for checking filtered year
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={changeFilterHandler}/>
+      {filteredExpenses.map((expense) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            date={expense.date}
+            price={expense.price}
+          />
+        );
+      })}
+    </Card>
+  );
+}
+
+export default Expenses;
